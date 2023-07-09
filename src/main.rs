c014eef69e1686611741c5013346b82eb0edafd0
Stefan Kupresak <stefan_vg@hotmail.com>
2023-07-10 00:21:15 +0200

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
-use std::io::prelude::*;
-use std::net::TcpListener;
+use std::io::{self, prelude::*};
+use std::net::{TcpListener, TcpStream};
 
-fn main() -> std::io::Result<()> {
+fn connection_loop(mut stream: TcpStream) -> io::Result<()> {
+    let mut buffer = [0; 1024];
+
+    while let Ok(n) = stream.read(&mut buffer) {
+        if n == 0 {
+            break; // connection was closed
+        }
+
+        stream.write(b"+PONG\r\n")?;
+    }
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                stream.write(b"+PONG\r\n")?;
+                connection_loop(stream)?;
             }
             Err(e) => {
                 println!("error: {}", e);
